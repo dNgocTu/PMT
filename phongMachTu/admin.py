d329e9b5d4989f912d9a26f3d209d649bc559477
DoPhi <huynhdophi473969@gmail.com>
2024-01-11 16:47:30 +0700

    thong ke

--- phongMachTu/admin.py
+++ phongMachTu/admin.py
@@ -1,19 +1,23 @@
 from phongMachTu.models import *
-from phongMachTu import app, db
+from phongMachTu import app, db, dao
 from flask_admin import Admin, BaseView, expose
 from flask_admin.contrib.sqla import ModelView
 from flask_login import logout_user, current_user
-from flask import redirect
+from flask import redirect, request
+
+
 class AuthenicatedAdmin(ModelView):
     def is_accessible(self):
         return current_user.is_authenticated and current_user.vaiTro == UserRoleEnum.ADMIN
+
 
 class AuthenicatedUser(BaseView):
     def is_accessible(self):
         return current_user.is_authenticated
 
+
 class MyQuyDinhView(AuthenicatedAdmin):
-    column_list = ["ten","giaTri"]
+    column_list = ["ten", "giaTri"]
     column_editable_list = ["giaTri"]
     details_modal = True
 
@@ -21,6 +25,8 @@ class MyQuyDinhView(AuthenicatedAdmin):
 class MyThuocView(AuthenicatedAdmin):
     form_columns = ['ten', 'donGia', 'soLuong', 'ngaySX', 'hanSD', 'donVi_id']
     column_list = ['ten', 'donGia', 'soLuong', 'ngaySX', 'hanSD']
+
+
 class MyLoaiThuocView(AuthenicatedAdmin):
     column_list = ["ten"]
     details_modal = True
@@ -34,10 +40,26 @@ class MyDonViThuocView(AuthenicatedAdmin):
 class MyNguoiDungView(AuthenicatedAdmin):
     column_list = ["ten", "vaiTro"]
 
+
 class StatsView(AuthenicatedUser):
-    @expose("/")
+    @expose('/')
     def index(self):
-        return self.render("admin/stats.html")
+        tongDoanhThu = 0
+        tongBenhNhan = 0
+        thang = request.args.get('thang01')
+        thang02 = request.args.get('thang02')
+        thong_ke_01 = dao.thong_ke_01(thang)
+        thong_ke_02 = dao.thong_ke_02(thang02)
+        for row in thong_ke_01:
+            tongDoanhThu += row.doanhThu
+            tongBenhNhan += row.SoBenhNhan
+        return self.render("admin/stats.html",
+                           thong_ke_01=thong_ke_01,
+                           tongDoanhThu=tongDoanhThu,
+                           tongBenhNhan=tongBenhNhan,
+                           thong_ke_02=thong_ke_02
+                           )
+
 
 class LogoutView(AuthenicatedUser):
     @expose("/")
@@ -46,8 +68,7 @@ class LogoutView(AuthenicatedUser):
         return redirect("/admin")
 
 
-
-admin = Admin(app=app, name="Phòng mạch tư", template_mode="bootstrap4" )
+admin = Admin(app=app, name="Phòng mạch tư", template_mode="bootstrap4")
 admin.add_view(MyQuyDinhView(QuyDinh, db.session, "Quy định"))
 admin.add_view(MyThuocView(Thuoc, db.session, "Thuốc"))
 admin.add_view(MyLoaiThuocView(LoaiThuoc, db.session, "Loại thuốc"))
@@ -56,4 +77,3 @@ admin.add_view(MyNguoiDungView(NguoiDung, db.session, "User"))
 
 admin.add_view(StatsView(name="Thống kê báo cáo"))
 admin.add_view(LogoutView(name="Đăng xuất"))
-
